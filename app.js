8e6f33b71b3164bcfa03dbd2760c836bca6a15d0
Suraj Kumar <surajsinhaking77@gmail.com>
2023-04-26 01:42:35 +0530

    adding some more

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))  // nessary to use when you using from to get the data
 app.set('view engine' , 'ejs');
 
 app.get('/', (req, res)=>{
     res.render('index');
+})
+
+app.post('/formSubmition', (req, res)=>{
+    // res.writeHead(200, {"Content-Type":'application/json'})
+    var data = req.body;
+    console.log(req.body);
+    res.send('index');
+    res.sendStatus(200);
 })
 
 
